f7bac3e0a8355aeb427ef62b99f663008bf80e4f
Iulia Feroli <ife@mediaan.com>
2018-03-27 10:48:36 +0200

    Put it in a function so we can call from different files - returns the "superclass" dictionary object

--- read_file.py
+++ read_file.py
@@ -1,26 +1,27 @@
 import string
 path = "C:/Users/ife/OneDrive - Mediaan abs b.v/UNI/block 4/Data analysis/data madness/"
 
-subtitles = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: []}
+def read_file(path):
 
-delete = '"0123456789:.,!?'
+	subtitles = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: []}
 
-for i in subtitles:
-	with open(path + "season" + str(i+1) + ".json") as infile:
-		episode = []
+	delete = '"0123456789:.,!?'
 
-		for line in infile:
-			if line.find(" }") > 0 :
-				subtitles[i].append(episode)
-				episode = []
-				next(infile)
-			elif line.find("{") < 0:
-				line = ''.join( c for c in line if  c not in delete + "'" ).strip()
-				episode.append(line)
+	for i in subtitles:
+		with open(path + "season" + str(i+1) + ".json") as infile:
+			episode = []
 
+			for line in infile:
+				if line.find(" }") > 0 :
+					subtitles[i].append(episode)
+					episode = []
+					next(infile)
+				elif line.find("{") < 0:
+					line = ''.join( c for c in line if  c not in delete + "'" ).strip()
+					episode.append(line)
 
+	#print (subtitles[2][5][3])
+	#subtitles [season 1: [ [episode], [episode], [episode] ], season2 [[] [] [] ], season 3 [[] [] [] ]
+	#count from 0 so print (subtitles[2][5][3]) season 3 episode 6 line 4
 
-print (subtitles[2][5][3])
-
-#subtitles [season 1: [ [episode], [episode], [episode] ], season2 [[] [] [] ], season 3 [[] [] [] ]
-#count from 0 so print (subtitles[2][5][3]) season 3 episode 6 line 4
+	return subtitles
