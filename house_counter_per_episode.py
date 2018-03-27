9da9f34332d187638a3609df53dee222751a6934
Iulia Feroli <ife@mediaan.com>
2018-03-27 14:01:09 +0200

    added a function for counting house name occurances.

--- house_counter_per_episode.py
+++ house_counter_per_episode.py
@@ -1,14 +1,12 @@
 import read_file
 from pandas import DataFrame as df
 
-# use data frames season column, episode columnd, 1 1 sort of keys
-
 subtitles = read_file.read_file(read_file.path)
 
 seasons = []
 episodes = []
 empty = []
-house_names = ["arryn" ,"baratheon" ,"frey" ,"greyjoy" ,"lannister" ,"martell" ,"stark" ,"targaryen" ,"tully" ,"tyrell" ]
+house_names = {"arryn":[] ,"baratheon":[] ,"frey":[] ,"greyjoy":[] ,"lannister":[] ,"martell":[] ,"stark":[] ,"targaryen":[] ,"tully":[],"tyrell":[] }
 
 for i,season in enumerate(subtitles):
 	for j,episode in enumerate(subtitles[season]):
@@ -16,9 +14,27 @@ for i,season in enumerate(subtitles):
 		episodes.append(j+1)
 		empty.append("")
 
+def count_house_occurances(line, house_names):
+	response = []
+	for name in house_names:
+		count = 0
+		for word in line.split():	
+			print (word)
+			if word.find(name) >= 0:
+				count += 1
+		response.append(count)
+	return response
+
+"""
+test = "stark stark baratheon frey nope maybe tully i love frey"
+print(count_house_occurances(test, house_names))
+"""
+
 data = {"season": seasons, "episode": episodes}
 for i,name in enumerate(house_names):
 	data[house_names[i]] = empty
 houses = df(data=data)
 print(houses)
+
+
 
